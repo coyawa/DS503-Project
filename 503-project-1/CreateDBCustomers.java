67dc54be08ff4d02b36eb92c969de5dd4cdc58c7
Congyang <avatar.wang@gmail.com>
2017-02-09 15:00:56 -0500

    Hello Java

---
+++ 503-project-1/CreateDBCustomers.java
@@ -0,0 +1,7 @@
+// CreateDBCustomers
+// By Jiexuan Sun & Congyang Wang
+
+// The Customers dataset should have the following attributes for each customer:
+// ID: unique sequential number (integer) from 1 to 50,000 (that is the file will have 50,000 line) Name: random sequence of characters of length between 10 and 20 (do not include commas) Age: random number (integer) between 10 to 70
+// CountryCode: random number (integer) between 1 and 10
+// Salary: random number (float) between 100 and 10000
---
+++ SomeTest/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main(String[] args) {
+		System.out.println("HelloWorld");
+	}
+}
+
+// https://guangyuwu.wordpress.com/2015/12/03/sublime-text-3-build-system-to-compile-run-java-on-mac/
